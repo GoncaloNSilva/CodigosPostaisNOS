6bfab58077ea1b39d9999320a8aefb2e41a93575
GoncaloSilvaUTAD <al60743@alunos.utad.pt>
2024-10-07 16:37:54 +0100

    implementação de conexão à base de dados, leitura do ficheiro csv e funcionalidade de fazer pedido à API dos CTT

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 # Press Ctrl+F5 to execute it or replace it with your code.
 import csv
 import json
-from configparser import ConfigParser
-
+import psycopg2
 import requests
-
-
-
+from psycopg2 import Error
 
 def main():
-    config = load_config()
-    # Use a breakpoint in the code line below to debug your script.
+    # connection = connect_db()
     api_key = "bfff75055c334aa1a1329ffdd5e8811d"
     codigo_postal = "2520-193"
     call_URL = "https://www.cttcodigopostal.pt/api/v1/" + api_key +"/"+ codigo_postal
     response = requests.get(call_URL)
     pretty_print_JSON(response)
-    read_and_print_csv()
+    result_csv = read_and_print_csv()
 
 def pretty_print_JSON(response):
     parsed = response.json()
@@ -25,23 +21,52 @@ def pretty_print_JSON(response):
 def read_and_print_csv():
     with open('codigos_postais.csv', 'r') as csvfile:
         parsed = csv.reader(csvfile, delimiter=" ")
+        next(parsed)
         for row in parsed:
-            print(f"Dados lidos do ficheiro csv: {parsed}")
+            row_parsed = row[0].split(",")
+            print(f"[0]:{row_parsed[0]}\n[1]:{row_parsed[1]}\n[2]:{row_parsed[2]}\n")
+            return
 
-def load_config(filename='db.ini', section='postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
+def connect_db():
+    try:
+        # Connect to an existing database
+        connection = psycopg2.connect(user="postgres",
+                                      password="12345",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="codigopostaldb")
+        cursor = connection.cursor()
+        # Print PostgreSQL details
+        print("PostgreSQL server information")
+        print(connection.get_dsn_parameters(), "\n")
+        create_table_query = ''' CREATE TABLE addresses (
+            id SERIAL PRIMARY KEY,
+            morada TEXT,
+            porta TEXT,
+            localidade TEXT,
+            freguesia TEXT,
+            concelho TEXT,
+            distrito TEXT,
+            latitude DECIMAL(10, 7),
+            longitude DECIMAL(10, 7),
+            codigo_postal VARCHAR(10),
+            info_local TEXT,
+            codigo_arteria VARCHAR(20),
+            concelho_codigo INTEGER,
+            distrito_codigo INTEGER);
+        '''
+        # Executing a SQL query
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Table created successfully in PostgreSQL ")
+    except (Exception, Error) as error:
+        print("Error while connecting to PostgreSQL", error)
 
-    # get section, default to postgresql
-    config = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            config[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return config
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
